6a75a3a10e7d299e38b6d8d2e87808dc6e8d75a9
JeremyJFarnsworth <jdog1123@gmail.com>
2022-09-29 21:19:55 -0600

    6th cors update

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ def send_email():
 def build_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
+    response.headers.add('Access-Control-Allow-Headers', "Content-Type")
+    response.headers.add('Access-Control-Allow-Methods', "POST, OPTIONS, GET, DELETE")
     response.headers.add('Content-Type', "application/json")
     return response
 
